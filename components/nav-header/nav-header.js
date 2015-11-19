1fcaf8542a5f803e7371452a2a785e8a5f90f60e
Radhika <radhika@Radhikas-MacBook-Pro.local>
2015-11-19 16:54:11 +0530

    Bug fix completed  on the nav-header

--- components/nav-header/nav-header.js
+++ components/nav-header/nav-header.js
@@ -1,7 +1,7 @@
 var app = angular.module('nav-header', []);
-
+ 
 app.directive('abNavHeader', function() {
-
+ 
   return {
     restrict: 'E',
     templateUrl:'/nav-header/nav-header.html',
@@ -9,16 +9,24 @@ app.directive('abNavHeader', function() {
       items: '=ngModel'
     },
     link: function (scope, element, attrs) {
-      scope.expand = function (item, evt) {
-        if (item.expanded) {
-          item.expanded = false;
-        }
-        else {
-          item.expanded = true;
-        }
-
+      scope.expand = function (evt) {
+        $(evt.currentTarget).siblings().each(function(){
+          $(this).find('.sub-header').removeClass('expand')
+        })
+        var subHeader = $(evt.currentTarget).find('.sub-header')[0]
+        $(subHeader).addClass('expand');
         evt.stopPropagation();
-      }
+      };
+ 
+      var collapseOthers = function (items) {
+        items.forEach(function (item) {
+          item.expanded = false;
+          if (item.categories && item.categories.length) {
+            collapseOthers(item.categories);
+          }
+        });
+      };
+ 
     }
    };
 });
