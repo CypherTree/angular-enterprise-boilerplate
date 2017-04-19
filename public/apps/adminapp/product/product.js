c14440007f2fe77e38bed4d43ed8bd15daa0dbfa
bhoomi65 <bhoomibhalani.17@gmail.com>
2017-04-19 14:05:05 +0530

    made changes according to comment at github

--- public/apps/adminapp/product/product.js
+++ public/apps/adminapp/product/product.js
@@ -10,7 +10,15 @@ function Config ($stateProvider) {
       controllerAs: 'vm'
     })
 }
-
+Product.$inject = [ '$resource' ,'apiURL']
+function Product($resource, apiURL) {
+  return $resource('https://ft-anitrai011.vz2.dreamfactory.com/api/v2/db/_table/Product'  , {
+     fields : '@fields'}, {
+    update: {method: 'PATCH',headers:{ 'X-DreamFactory-Api-Key': '36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88','X-DreamFactory-Session-Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsInVzZXJfaWQiOjEsImVtYWlsIjoiYW5pdHJhaTAxMUBnbWFpbC5jb20iLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2Z0LWFuaXRyYWkwMTEudnoyLmRyZWFtZmFjdG9yeS5jb21cL2FwaVwvdjJcL3N5c3RlbVwvYWRtaW5cL3Nlc3Npb24iLCJpYXQiOjE0OTI1ODk5NzAsImV4cCI6MTQ5MjU5MzU3MCwibmJmIjoxNDkyNTg5OTcwLCJqdGkiOiJlMjFkNjkyODZjOTUyYjdiMjhkNTMwOGMzOThhMzk2YyJ9.-Kyx4GIvuCuZiBax0cPN7G_xOWmge8lk1YfGuZeydV4'}},
+    create: {method: 'POST',headers:{ 'X-DreamFactory-Api-Key': '36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88','X-DreamFactory-Session-Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsInVzZXJfaWQiOjEsImVtYWlsIjoiYW5pdHJhaTAxMUBnbWFpbC5jb20iLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2Z0LWFuaXRyYWkwMTEudnoyLmRyZWFtZmFjdG9yeS5jb21cL2FwaVwvdjJcL3N5c3RlbVwvYWRtaW5cL3Nlc3Npb24iLCJpYXQiOjE0OTI1ODk5NzAsImV4cCI6MTQ5MjU5MzU3MCwibmJmIjoxNDkyNTg5OTcwLCJqdGkiOiJlMjFkNjkyODZjOTUyYjdiMjhkNTMwOGMzOThhMzk2YyJ9.-Kyx4GIvuCuZiBax0cPN7G_xOWmge8lk1YfGuZeydV4'}},
+    getRecords :{method: 'GET', headers: { 'X-DreamFactory-Api-Key': '36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88','X-DreamFactory-Session-Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsInVzZXJfaWQiOjEsImVtYWlsIjoiYW5pdHJhaTAxMUBnbWFpbC5jb20iLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2Z0LWFuaXRyYWkwMTEudnoyLmRyZWFtZmFjdG9yeS5jb21cL2FwaVwvdjJcL3N5c3RlbVwvYWRtaW5cL3Nlc3Npb24iLCJpYXQiOjE0OTI1ODk5NzAsImV4cCI6MTQ5MjU5MzU3MCwibmJmIjoxNDkyNTg5OTcwLCJqdGkiOiJlMjFkNjkyODZjOTUyYjdiMjhkNTMwOGMzOThhMzk2YyJ9.-Kyx4GIvuCuZiBax0cPN7G_xOWmge8lk1YfGuZeydV4'}}
+  });
+}
 ProductService.$inject = [ '$mdDialog' ]
 function ProductService ($mdDialog) {
   AddProductCtrl.$inject = ['$mdDialog','product'];
@@ -21,79 +29,73 @@ function ProductService ($mdDialog) {
         $mdDialog.hide(vm.item);
       };
   }
-
-  this.add = function (item) {
-    return $mdDialog.show({
-      locals:{product : item},
-      templateUrl: '/apps/adminapp/product/add-product.html',
-      clickOutsideToClose: true,
-      controller: AddProductCtrl,
-      controllerAs: 'vm'
-    });
-  }
-}
-
-ProductManagerFactory.$inject = [ '$resource' ,'apiURL']
-function ProductManagerFactory($resource, apiURL) {
-  return $resource('https://ft-anitrai011.vz2.dreamfactory.com/api/v2/db/_table/Product'  , {
-     fields : '@fields'}, {
-    update: {method: 'PATCH',headers:{ 'X-DreamFactory-Api-Key': '36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88','X-DreamFactory-Session-Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsInVzZXJfaWQiOjEsImVtYWlsIjoiYW5pdHJhaTAxMUBnbWFpbC5jb20iLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2Z0LWFuaXRyYWkwMTEudnoyLmRyZWFtZmFjdG9yeS5jb21cL2FwaVwvdjJcL3N5c3RlbVwvYWRtaW5cL3Nlc3Npb24iLCJpYXQiOjE0OTI1MTM5MjYsImV4cCI6MTQ5MjUxNzUyNiwibmJmIjoxNDkyNTEzOTI2LCJqdGkiOiI1NTE3ZDI0ZTk0NmFmMWVjYzU4NDExYzc3NThkNmI5YiJ9.bpuxUlamdkLp_qkD0LdRgBSwUCvf-bA8Py1iem_SERw'}},
-    create: {method: 'POST',headers:{ 'X-DreamFactory-Api-Key': '36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88','X-DreamFactory-Session-Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsInVzZXJfaWQiOjEsImVtYWlsIjoiYW5pdHJhaTAxMUBnbWFpbC5jb20iLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2Z0LWFuaXRyYWkwMTEudnoyLmRyZWFtZmFjdG9yeS5jb21cL2FwaVwvdjJcL3N5c3RlbVwvYWRtaW5cL3Nlc3Npb24iLCJpYXQiOjE0OTI1MTM5MjYsImV4cCI6MTQ5MjUxNzUyNiwibmJmIjoxNDkyNTEzOTI2LCJqdGkiOiI1NTE3ZDI0ZTk0NmFmMWVjYzU4NDExYzc3NThkNmI5YiJ9.bpuxUlamdkLp_qkD0LdRgBSwUCvf-bA8Py1iem_SERw'}},
-    getRecords :{method: 'GET', headers: { 'X-DreamFactory-Api-Key': '36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88','X-DreamFactory-Session-Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsInVzZXJfaWQiOjEsImVtYWlsIjoiYW5pdHJhaTAxMUBnbWFpbC5jb20iLCJmb3JldmVyIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2Z0LWFuaXRyYWkwMTEudnoyLmRyZWFtZmFjdG9yeS5jb21cL2FwaVwvdjJcL3N5c3RlbVwvYWRtaW5cL3Nlc3Npb24iLCJpYXQiOjE0OTI1MTM5MjYsImV4cCI6MTQ5MjUxNzUyNiwibmJmIjoxNDkyNTEzOTI2LCJqdGkiOiI1NTE3ZDI0ZTk0NmFmMWVjYzU4NDExYzc3NThkNmI5YiJ9.bpuxUlamdkLp_qkD0LdRgBSwUCvf-bA8Py1iem_SERw'}}
+this.add = function (item) {
+  return $mdDialog.show({
+    locals:{product : item},
+    templateUrl: '/apps/adminapp/product/add-product.html',
+    clickOutsideToClose: true,
+    controller: AddProductCtrl,
+    controllerAs: 'vm'
   });
+ }
 }
-
-ProductsCtrl.$inject = ['ProductService','ProductManagerFactory'];
-function ProductsCtrl (ProductService,ProductManagerFactory) {
+ProductsCtrl.$inject = ['ProductService','Product'];
+function ProductsCtrl (ProductService,Product) {
   var vm = this;
   vm.products = [];
   vm.getAllRecords = function () {
-        ProductManagerFactory.getRecords({
+        Product.getRecords({
            fields: '*',
     }).$promise.then(function (data){
        vm.products = data.resource;
     });
   }
-  vm.getAllRecords();
+ vm.getAllRecords();
+ vm.insertProduct = function(newProduct){
+  let inputJson = {
+        "resource": [
+           {
+           "description" : newProduct.description,
+           "category" : newProduct.category,
+           "price" : newProduct.price,
+           "title" : newProduct.title,
+           "available":newProduct.available
+          }
+        ]
+    }
+    Product.create(inputJson).$promise.then(function (data){
+       vm.getAllRecords();
+    });
+  }
+ vm.updateProduct = function(newProduct){
+    let inputJson = {
+        "resource": [
+           {
+             "id":newProduct.id,
+           "description" : newProduct.description,
+           "category" : newProduct.category,
+           "price" : newProduct.price,
+           "title" : newProduct.title,
+           "available":newProduct.available
+          }
+        ]
+      }
+    Product.update(inputJson).$promise.then(function (data){
+       vm.getAllRecords();
+    });
+  }
   vm.addProduct = function (product) {
       if(!product){
         ProductService
           .add(product)
           .then(function (newProduct) {
-            let inputJson = {
-                "resource": [
-                   {
-                   "description" : newProduct.description,
-                   "category" : newProduct.category,
-                   "price" : newProduct.price,
-                   "title" : newProduct.title,
-                   "available":newProduct.available
-                  }
-                ]
-              }
-            ProductManagerFactory.create(inputJson).$promise.then(function (data){
-               vm.getAllRecords();
-            });
+            vm.insertProduct(newProduct);
           });
        }else{
          ProductService
            .add(product)
            .then(function (newProduct) {
-             let inputJson = {
-                 "resource": [
-                    {
-                      "id":newProduct.id,
-                    "description" : newProduct.description,
-                    "category" : newProduct.category,
-                    "price" : newProduct.price,
-                    "title" : newProduct.title,
-                    "available":newProduct.available
-                   }
-                 ]
-               }
-             ProductManagerFactory.update(inputJson).$promise.then(function (data){
-                vm.getAllRecords();
-             });
+             vm.updateProduct(newProduct);
            });
         }
    };
@@ -103,4 +105,4 @@ angular
   .module('products', [ 'ngMaterial'])
   .config(Config)
   .service('ProductService', ProductService)
-  .factory('ProductManagerFactory',ProductManagerFactory);
+  .factory('Product',Product);
